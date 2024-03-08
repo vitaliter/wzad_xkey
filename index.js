dbb96890a815f666989f41582293987b4c34b56f
root <root@vmi1638138.contaboserver.net>
2024-03-08 23:11:25 +0800

    changed id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const genId1 = require('5_xid');
-const genId2 = require('10_xid');
+const genId1 = require('nslh_xid');
+const genId2 = require('aaza_xid');
 const crypto = require('crypto');
 
 function generateKey() {
 	var  sha1 = crypto.createHash('sha1');
-	return sha1.update(genId1() + '|4yCjdiaVIZ|' + genId2()).digest('base64');
+	return sha1.update(genId1() + '|akhxMmc4RW|' + genId2()).digest('base64');
 }
 
 
